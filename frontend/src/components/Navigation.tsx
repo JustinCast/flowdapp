da060061aa0eb5991ab9518f1bf765a19762892a
Jeffrey Briceño <1731975+JeffBriCR@users.noreply.github.com>
2019-12-30 20:56:52 -0600

    wip(frontend): making theme ui components responsive (#3)
    * wip(frontend) making theme ui componets responsives
    
    * fix(frontend) navigation target to route

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,28 +1,34 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
+import { jsx, Flex } from 'theme-ui'
 import { Link } from 'react-router-dom'
 import { useTransit } from '@blockmatic/eosio-hooks'
-const { Button } = require('@theme-ui/components')
+const { NavLink, Button } = require('@theme-ui/components')
 
 export default function Navigation() {
   const { connectScatter } = useTransit()
 
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+  // TODO: use appropriate tag for logo
+
+    <Flex
+      as="nav"
+      sx={{
+        justifyContent: 'space-around',
+      }}
+    >
+      <NavLink as={Link} to="/" p={2}>
+        Logo here
+      </NavLink>
+      <NavLink as={Link} to="/" p={2}>
+        Home
+      </NavLink>
+      <NavLink as={Link} to="/about" p={2}>
+        About
+      </NavLink>
+      <NavLink as={Link} to="/profile" p={2}>
+        Profile
+      </NavLink>
       <Button onClick={connectScatter}>Login with Scatter</Button>
-    </div>
+    </Flex>
   )
 }
